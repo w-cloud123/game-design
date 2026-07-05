e526fb91d61302e4e32c600a0aec19ef280ec5fd
w-cloud123 <2831756382@qq.com>
2026-07-05 21:27:30 +0800

    Delete AudioManager.cpp

--- AudioManager.cpp
+++
@@ -1,9 +0,0 @@
-// ============================================================
-// src/AudioManager.cpp
-// ============================================================
-#include "AudioManager.hpp"
-
-AudioManager& AudioManager::getInstance() {
-    static AudioManager instance;
-    return instance;
-}
